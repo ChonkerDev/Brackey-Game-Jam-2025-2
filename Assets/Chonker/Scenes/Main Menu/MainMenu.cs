94f319df041a524330bac2484d464049b03a5fc3
ChonkerDev <163789588+ChonkerDev@users.noreply.github.com>
2025-08-26 13:30:03 -0500

    Basic Main Menu logic

--- Assets/Chonker/Scenes/Main Menu/MainMenu.cs
+++ Assets/Chonker/Scenes/Main Menu/MainMenu.cs
@@ -1,16 +1,31 @@
+using System.Collections;
+using Chonker.Core.Tween;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private CanvasGroup PressAnyKeyCanvasGroup;
+    [SerializeField] private CanvasGroup MainMenuCanvasGroup;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField] private AnimationCurve MainMenuPopInScaleCurve;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    IEnumerator Start() {
+        RectTransform mainMenuRectTransform = MainMenuCanvasGroup.GetComponent<RectTransform>();
+        mainMenuRectTransform.localScale = Vector3.zero;
+        MainMenuCanvasGroup.gameObject.SetActive(false);
+
+        while (!Keyboard.current.anyKey.wasPressedThisFrame) {
+            yield return null;
+        }
+
+
+        MainMenuCanvasGroup.gameObject.SetActive(true);
+        yield return StartCoroutine(TweenCoroutines.RunTaper(.5f,
+            f => { PressAnyKeyCanvasGroup.alpha = 1 - f; },
+            () => { PressAnyKeyCanvasGroup.gameObject.SetActive(false); }));
+        StartCoroutine(TweenCoroutines.RunAnimationCurveTaper(.5f, MainMenuPopInScaleCurve,
+            curveAlpha => { mainMenuRectTransform.localScale = Vector3.one * curveAlpha; }));
     }
 }
