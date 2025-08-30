b4af3fb38bb01501a1193d7dfd16889d87f71b53
ChonkerDev <163789588+ChonkerDev@users.noreply.github.com>
2025-08-30 16:42:21 -0500

    Enemy detection indicator, main menu music, other improvements

---
+++ Assets/Chonker/Scripts/Enemy/EnemyDetectionIndicator.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using Chonker.Core.Tween;
+using Chonker.Scripts.Enemy.Enemy_State;
+using UnityEngine;
+
+public class EnemyDetectionIndicator : MonoBehaviour
+{
+    [SerializeField] private EnemyAiStateManager _enemyAiStateManager;
+
+    [SerializeField] private SpriteRenderer _detectSpriteRenderer;
+
+    [SerializeField] private SpriteRenderer _lostSpriteRenderer;
+
+    [SerializeField] private AnimationCurve _showIconAnimationCurve;
+    [SerializeField] private AudioSource _exclamationAudioSource;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
+        _detectSpriteRenderer.transform.localScale = Vector3.zero;
+        _lostSpriteRenderer.transform.localScale = Vector3.zero;
+        _enemyAiStateManager.OnStateChange.AddListener((old, current) => {
+            StopAllCoroutines();
+            _detectSpriteRenderer.transform.localScale = Vector3.zero;
+            _lostSpriteRenderer.transform.localScale = Vector3.zero;
+            bool lostDetection = old == EnemyStateId.Chase && current == EnemyStateId.Patrol;
+            bool enteredDetection = current == EnemyStateId.Chase && old == EnemyStateId.Patrol;
+            if (lostDetection) {
+                StartCoroutine(TweenCoroutines.RunAnimationCurveTaper(.3f, _showIconAnimationCurve, f => {
+                        _lostSpriteRenderer.transform.localScale = Vector3.one * f;
+                    }, false,
+                    () => { StartCoroutine(delayHide(_lostSpriteRenderer)); }));
+            }
+
+            if (enteredDetection) {
+                StartCoroutine(TweenCoroutines.RunAnimationCurveTaper(.3f, _showIconAnimationCurve, f => {
+                        _detectSpriteRenderer.transform.localScale = Vector3.one * f;
+                    }, false,
+                    () => { StartCoroutine(delayHide(_detectSpriteRenderer)); }));
+                _exclamationAudioSource.Play();
+            }
+        });
+    }
+
+    private void FixedUpdate() {
+        transform.rotation = Quaternion.identity;
+    }
+
+
+    private IEnumerator delayHide(SpriteRenderer spriteRenderer) {
+        yield return new WaitForSeconds(2);
+        spriteRenderer.transform.localScale = Vector3.zero;
+    }
+}
